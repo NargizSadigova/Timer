b0951e01fe94171b671a424ee1866602df27b3c3
NargizSadigova <nargizsadigova1304@gmail.com>
2024-12-11 16:01:58 +0400

    time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import {Routes, Route, Link, NavLink} from 'react-router-dom';
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
 import Watch from './Component/Watch';
 import Timer from './Component/Timer';
 import Counter from './Component/Counter';
 
 function App() {
-  
+
   return (
     <div className="App">
       <div className='timeline'>
-      <Link className='settings' to='/watch'>Watch</Link>
-      <Link className='settings' to='/timer'>Timer</Link>
-      <Link className='settings' to='/counter'>Counter</Link>
+        <Link className='settings' to='/watch'>Watch</Link>
+        <Link className='settings' to='/timer'>Timer</Link>
+        <Link className='settings' to='/counter'>Counter</Link>
       </div>
 
-     <Routes>
-       
+      <Routes>
+
         <Route path='/watch' element={<Watch />} />
         <Route path='/timer' element={<Timer />} />
         <Route path='/counter' element={<Counter />} />
-     </Routes>
+      </Routes>
     </div>
   )
 }
